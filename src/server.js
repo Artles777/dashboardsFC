5442d733b8e2ec0e5d43c3be5688d55dce585de2
Artem Alesenko <62352758+Artles777@users.noreply.github.com>
2021-03-25 06:04:47 +0300

    pre server api

--- src/server.js
+++ src/server.js
@@ -1,77 +1,6 @@
-// const path = require('path');
-// const fs = require('fs');
-// const readline = require('readline');
-// const { google } = require('googleapis');
-//
-// const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-// const TOKEN_PATH = 'token.json';
-//
-// function getNewToken(oAuth2Client, callback) {
-//   const authUrl = oAuth2Client.generateAuthUrl({
-//     access_type: 'offline',
-//     scope: SCOPES,
-//   });
-//   console.log('Authorize this app by visiting this url:', authUrl);
-//   const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-//   });
-//   rl.question('Enter the code from that page here: ', (code) => {
-//     rl.close();
-//     oAuth2Client.getToken(code, (err, token) => {
-//       if (err) console.error('Error while trying to retrieve access token', err);
-//       oAuth2Client.setCredentials(token);
-//       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (error) => {
-//         if (error) console.error(error);
-//         console.log('Token stored to', TOKEN_PATH);
-//       });
-//       callback(oAuth2Client);
-//     });
-//   });
-// }
-//
-// function listMajors(auth) {
-//   const sheets = google.sheets({ version: 'v4', auth });
-//   sheets.spreadsheets.values.get({
-//     spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-//     range: 'Class Data!A2:E',
-//   }, (err, res) => {
-//     if (err) {
-//       console.log(`The API returned an error: ${err}`);
-//     }
-//     const rows = res.data.values;
-//     if (rows.length) {
-//       console.log('Name, Major:');
-//       rows.map((row) => console.log(`${row[0]}, ${row[4]}`));
-//     } else {
-//       console.log('No data found.');
-//     }
-//   });
-// }
-//
-// function authorize(credentials, callback) {
-//   const oAuth2Client = new google.auth.OAuth2(
-//     credentials.installed.client_id,
-//     credentials.installed.client_secret,
-//     credentials.installed.redirect_uris[0],
-//   );
-//
-//   fs.readFile(TOKEN_PATH, (err, token) => {
-//     if (err) getNewToken(oAuth2Client, callback);
-//     oAuth2Client.setCredentials(JSON.parse(token));
-//     callback(oAuth2Client);
-//   });
-// }
-//
-// fs.readFile(path.resolve(__dirname, 'credentials.json'), (err, content) => {
-//   if (err) return console.log('Error loading client secret file:', err);
-//   return authorize(JSON.parse(content), listMajors);
-// });
-
 const path = require('path');
 const fs = require('fs');
-const readline = require('readline');
-const { google } = require('googleapis');
+const excelToJson = require('convert-excel-to-json');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -82,93 +11,93 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-const TOKEN_PATH = path.resolve(__dirname, 'token.json');
-
-function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) console.error('Error while trying to retrieve access token', err);
-      oAuth2Client.setCredentials(token);
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (error) => {
-        if (error) console.error(error);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
-
-function listMajors(auth) {
-  const sheets = google.sheets({ version: 'v4', auth });
-  sheets.spreadsheets.values.get({
-    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-    range: 'Class Data!A2:E',
-  }, (err, res) => {
-    if (err) {
-      console.log(`The API returned an error: ${err}`);
-    }
-    const rows = res.data.values;
-    if (rows.length) {
-      console.log('Name, Major:');
-      rows.map((row) => console.log(`${row[0]}, ${row[4]}`));
-    } else {
-      console.log('No data found.');
-    }
-  });
-}
-
-function authorize(credentials, callback) {
-  const oAuth2Client = new google.auth.OAuth2(
-    credentials.installed.client_id,
-    credentials.installed.client_secret,
-    credentials.installed.redirect_uris[0],
-  );
-
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}
-
-fs.readFile(path.resolve(__dirname, 'credentials.json'), (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  return authorize(JSON.parse(content), listMajors);
+const resultExcel = excelToJson({
+  source: fs.readFileSync(path.resolve(__dirname, 'listDashboards.xlsx')),
+  columnToKey: {
+    A: 'department',
+    E: 'name',
+    I: 'completed',
+    J: 'working',
+  },
 });
-// app.get('/', (req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'application/json' });
-//   const readStream = fs.createReadStream(
-//   path.resolve(__dirname, 'listDashboards.xlsx'), 'utf-8'
-//   );
-//   // const result = excelToJson({
-//   //   sourceFile: ,
-//   //   header: {
-//   //     rows: 1,
-//   //   },
-//   readStream.on('data', () => {
-//     readStream.pipe(res);
-//   });
-// });
 
-// app.get('/', (req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'application/json' });
-//   const readStream = fs.createReadStream(
-//     path.resolve(__dirname, 'listDashboards.xlsx'), { encoding: 'base64' },
-//   );
-//   // res.send(readStream);
-//   readStream.on('data', () => readStream.pipe(res));
-// });
+const mapJson = resultExcel['Лист1'].slice(1);
+const newJsonExcel = {};
+let department;
+let name;
+mapJson.forEach((row) => {
+  if (department !== row.department) {
+    department = row.department;
+    newJsonExcel[department] = {};
+  }
+  if (name !== row.name) {
+    name = row.name;
+    newJsonExcel[department][name] = {};
+    newJsonExcel[department][name].completed = 0;
+    newJsonExcel[department][name].working = 0;
+  }
+  if ([name].completed !== row.completed) {
+    newJsonExcel[department][name].completed += row.completed;
+    newJsonExcel[department][name].working += row.working;
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send(newJsonExcel);
+});
+
+app.get('/departments', (req, res) => {
+  res.send(Object.keys(newJsonExcel));
+});
+
+app.get('/departments/:id', (req, res) => {
+  const { id } = req.params;
+  const dep = Object.keys(newJsonExcel);
+  res.send(dep[id]);
+});
+
+app.get('/names', (req, res) => {
+  const names = [...new Set(mapJson.map((row) => row.name))];
+  res.send(names);
+});
+
+app.get('/names/:id', (req, res) => {
+  const { id } = req.params;
+  const names = [...new Set(mapJson.map((row) => row.name))];
+  res.send(names[id]);
+});
+
+app.get('/departments-names', (req, res) => {
+  const names = Object.values(newJsonExcel).map((n) => Object.keys(n));
+  const depNames = Object.keys(newJsonExcel);
+  const result = depNames.map((n, idx) => ({
+    [n]: names[idx],
+  }));
+  res.send(result);
+});
+
+app.get('/departments/names/:id', (req, res) => {
+  const { id } = req.params;
+  const names = Object.values(newJsonExcel).map((n) => Object.keys(n));
+  res.send(id <= names.length ? names[id] : names[0]);
+});
+
+app.get('/completed', (req, res) => {
+  const completed = Object.values(newJsonExcel)
+    .map((n) => Object.values(n))
+    .map((dep) => dep.map((value) => value.completed))
+    .map((sum) => sum.reduce((acc, n) => acc + n));
+  res.send(completed);
+});
+
+app.get('/completed/:id', (req, res) => {
+  const { id } = req.params;
+  const completed = Object.values(newJsonExcel)
+    .map((n) => Object.values(n))
+    .map((dep) => dep.map((value) => value.completed))
+    .map((sum) => sum.reduce((acc, n) => acc + n, 0));
+  res.send(completed.slice(1));
+});
 
 app.listen(PORT, () => {
   console.log(`Server run on http://localhost:${PORT}/`);
